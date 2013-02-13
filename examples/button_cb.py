10f3258878316c4960fead8e8b33c15c8ed7c489
Alejandro Autalán <alejandroautalan@gmail.com>
2013-02-13 02:11:37 -0300

    Update version to 0.3 Fix some examples.

--- examples/button_cb.py
+++ examples/button_cb.py
@@ -15,7 +15,7 @@ class Myapp:
         fpath = os.path.join(os.path.dirname(__file__),"button_cb.ui")
         builder.add_from_file(fpath)
 
-        mainwindow = builder.get_object('mainwindow1')
+        mainwindow = builder.get_object('mainwindow')
         mainwindow.grid()
 
         builder.connect_commands(self)
