4c661e1e2cd149b374e842a489d2551e932461e0
Alejandro Autalán <alejandroautalan@gmail.com>
2013-07-12 01:06:06 -0300

    Add default images. Add expanded option.

--- pygubu/widgets/accordionframe.py
+++ pygubu/widgets/accordionframe.py
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 #
 # Copyright 2012-2013 Alejandro Autalán
 #
@@ -24,6 +25,16 @@ except:
     import Tkinter as tk
     import ttk
 
+img_down = '''\
+R0lGODlhEAAQAIAAAAAAAAAAACH+EUNyZWF0ZWQgd2l0aCBHSU1QACH5BAEKAAEALAAAAAAQABAA
+AAIXjI+py+0P4wK0WprunRo0/VgRJpXmyRQAOw==
+'''
+
+img_right = '''\
+R0lGODlhEAAQAIAAAAAAAAAAACH+EUNyZWF0ZWQgd2l0aCBHSU1QACH5BAEKAAEALAAAAAAQABAA
+AAIdjI+pywGtwINHTmpvy3rxnnwQh1mUI52o6rCuWgAAOw==
+'''
+
 
 class AccordionFrame(ttk.Frame):
     """ An accordion like widget.
@@ -34,24 +45,34 @@ class AccordionFrame(ttk.Frame):
         label = ttk.Label(g, text='Label on group1')
         label.grid()
     """
+    IMAGES = None
 
     def __init__(self, master=None, **kw):
-        tk.Frame.__init__(self, master, **kw)
+        ttk.Frame.__init__(self, master, **kw)
+        self.__images = None
         self.__groups = {}
         self.columnconfigure(0, weight=1)
 
+        if AccordionFrame.IMAGES is None:
+            AccordionFrame.IMAGES = [
+                tk.PhotoImage(data=img_down),
+                tk.PhotoImage(data=img_right),]
+        self.__images = AccordionFrame.IMAGES
 
-    def add_group(self, gid, label=None):
+
+
+    def add_group(self, gid, label=None, expanded=True):
 
         glabel = label
         if label is None:
             glabel = str(gid)
 
         #button creation
-        btn = ttk.Button(self, text=glabel, style='DDFButton.TButton')
+        btn = ttk.Button(self, text=glabel, style='Toolbutton',
+            image=self.__images[0], compound='left')
         btn.grid(sticky=tk.EW)
         btn.dd_show = True
-        btn.configure(command=lambda:self.on_grp_button_clicked(gid))
+        btn.configure(command=lambda:self.__button_clicked(gid))
 
         #frame creation
         frame = ttk.Frame(self, width=100, height=100)
@@ -59,30 +80,53 @@ class AccordionFrame(ttk.Frame):
         frame.rowconfigure(0, weight=1)
         frame.columnconfigure(0, weight=1)
 
-        #Configure resizing of rows
-        #position = len(self.__groups)  + 1
-        #self.rowconfigure(position, weight=1)
+        if not expanded:
+            self.after_idle(lambda: self.__button_clicked(gid))
 
         #store button, and frame
         self.__groups[gid] = (btn, frame)
 
         return frame
 
-    def on_grp_button_clicked(self, gid):
+
+    def get_group(self, gid):
+        return self.__groups[gid][1]
+
+
+    def __button_clicked(self, gid):
         btn, frame = self.__groups[gid]
         if btn.dd_show == True:
             btn.dd_show = False
+            btn.configure(image=self.__images[1])
             frame.grid_remove()
         else:
             btn.dd_show = True
+            btn.configure(image=self.__images[0])
             frame.grid()
+
+    def set_images(self, img_open, img_close):
+        if self.__images == AccordionFrame.IMAGES:
+            self.__images = [
+                tk.PhotoImage(file=img_open),
+                tk.PhotoImage(file=img_close),]
+        else:
+            self.__images[0].configure(file=img_open)
+            self.__images[1].configure(file=img_close)
+
+
+    @classmethod
+    def set_gimages(cls, img_open, img_close):
+        if cls.IMAGES is None:
+            cls.IMAGES = [
+                tk.PhotoImage(file=img_open),
+                tk.PhotoImage(file=img_close),]
+        else:
+            cls.IMAGES[0].configure(file=img_open)
+            cls.IMAGES[1].configure(file=img_close)
 
 
 if __name__ == '__main__':
     root = tk.Tk()
-
-    s = ttk.Style()
-    s.configure('DDFButton.TButton', anchor=tk.W)
 
     app = AccordionFrame(root)
     app.grid(sticky=tk.NSEW)
@@ -91,17 +135,24 @@ if __name__ == '__main__':
     top.rowconfigure(0, weight=1)
     top.columnconfigure(0, weight=1)
 
-    g = app.add_group('g1', 'Group1')
+    g = app.add_group('g1', 'Tk widgets')
     l = tk.Label(g, text="Label1")
     l.grid()
     l = tk.Label(g, text="Label2")
     l.grid()
-    g = app.add_group('g2', 'Group2')
+    g = app.add_group('g2', 'Ttk widgets')
+    ##
+    app = AccordionFrame(g)
+    app.grid(sticky='nsew', padx="5 0")
+    g = app.add_group('g1', 'Containers')
+    l = tk.Label(g, text="Label1")
+    l.grid()
+    l = tk.Label(g, text="Label2")
+    l.grid()
+    g = app.add_group('g2', 'Control')
     l = tk.Label(g, text="Label3")
     l.grid()
     l = tk.Label(g, text="Label4")
     l.grid()
 
     tk.mainloop()
-
-
