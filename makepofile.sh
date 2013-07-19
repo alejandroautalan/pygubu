6889f652561dada3d5a80137c3f595176fb01954
Alejandro Autalán <alejandroautalan@gmail.com>
2013-07-19 03:56:21 -0300

    fix pot maker.

--- makepofile.sh
+++ makepofile.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-xgettext -L glade --output=po/pygubu.pot $(find ./pygubu/uidesigner/ui -name "*.ui")
-xgettext --join-existing --language=Python --keyword=_ --output=po/pygubu.pot --from-code=UTF-8 `find ./pygubu/uidesigner -name "*.py"`
+xgettext -L glade --output=po/pygubu.pot $(find ./pygubudesigner/ui -name "*.ui")
+xgettext --join-existing --language=Python --keyword=_ --output=po/pygubu.pot --from-code=UTF-8 `find ./pygubudesigner -name "*.py"`
