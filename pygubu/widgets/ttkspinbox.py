deff306e02a678b7933f6470e911047703e075b7
Alejandro Autalán <alejandroautalan@gmail.com>
2014-01-12 19:39:49 -0300

    Class for support tk8.6 ttk::spinbox

---
+++ pygubu/widgets/ttkspinbox.py
@@ -0,0 +1,12 @@
+import tkinter.ttk as ttk
+
+class Spinbox(ttk.Entry):
+
+    def __init__(self, master=None, **kw):
+        ttk.Entry.__init__(self, master, "ttk::spinbox", **kw)
+
+    def current(self, newindex=None):
+        return self.tk.call(self._w, 'current', newindex)
+
+    def set(self, value):
+        return self.tk.call(self._w, 'set', value)
