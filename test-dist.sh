2466976cc49e8176f6e243e0f15a8735b2f52b33
Alejandro Autalán <alejandroautalan@gmail.com>
2014-06-17 09:47:55 -0300

    Update dist test script.

--- test-dist.sh
+++ test-dist.sh
@@ -1,3 +1,4 @@
+rm dist/* -rf
 python setup.py sdist
-cd dist/; tar xvzf pygubu-0.7.tar.gz ; cd pygubu-0.7; cpython2.6 setup.py install; cd ..; cd ..;
-python ~/apps/python26.custom/bin/pygubu 
+cd dist/; tar xvzf pygubu-*.tar.gz ; cd pygubu-*; cpython3.4 setup.py install; cd ..; cd ..;
+cpython3.4 ~/apps/cpython-3.4.0/bin/pygubu-designer
