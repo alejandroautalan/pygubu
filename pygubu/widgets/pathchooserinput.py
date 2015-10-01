b69862a91b20f8d671e0ced7c5b9e4597e40a4d8
Alejandro Autalán <alejandroautalan@gmail.com>
2015-10-01 16:47:52 -0300

    Generate '<<PathChooserPathChanged>>' event on focusout. fixes #60

--- pygubu/widgets/pathchooserinput.py
+++ pygubu/widgets/pathchooserinput.py
@@ -1,3 +1,4 @@
+# encoding: utf8
 from __future__ import unicode_literals
 
 try:
@@ -11,16 +12,24 @@ except:
 
 
 class PathChooserInput(ttk.Frame):
+    """ Allows to choose a file or directory.
+    
+    Generates <<PathChooserPathChanged>> event when the path is changed.
+    
+    """
+
     FILE = 'file'
     DIR = 'directory'
 
     def __init__(self, master=None, **kw):
         ttk.Frame.__init__(self, master, **kw)
         self._choose = self.FILE
+        self._oldvalue = ''
         # subwidgets
         self.entry = o = ttk.Entry(self)
         o.grid(row=0, column=0, sticky='ew')
         o.bind('<KeyPress>', self.__on_enter_key_pressed)
+        o.bind('<FocusOut>', self.__on_focus_out)
         self.folder_button = o = ttk.Button(self, text='▶', command = self.__on_folder_btn_pressed, width=4)
         o.grid(row=0, column=1, padx=2)
 
@@ -41,11 +50,12 @@ class PathChooserInput(ttk.Frame):
         if key in args:
             self.entry.delete(0, 'end')
             self.entry.insert(0, args[key])
-            self.event_generate('<<PathChooserPathChanged>>')
+            self._generate_changed_event()
             del args[key]
         key = 'textvariable'
         if key in args:
             self.entry.configure(textvariable=args[key])
+            self._generate_changed_event()
             del args[key]
         ttk.Frame.configure(self, args)
 
@@ -65,11 +75,25 @@ class PathChooserInput(ttk.Frame):
         if key == option:
             return self.entry.cget(option)
         return ttk.Frame.cget(self, key)
+        
+    def _is_changed(self):
+#        print(repr(self._oldvalue), ':', repr(self.entry.get()))
+        if self._oldvalue != self.entry.get():
+            return True
+        return False
+    
+    def _generate_changed_event(self):
+        if self._is_changed():
+            self._oldvalue = self.entry.get()
+            self.event_generate('<<PathChooserPathChanged>>')
 
     def __on_enter_key_pressed(self, event):
         key = event.keysym
         if key in  ('Return','KP_Enter'):
-            self.event_generate('<<PathChooserPathChanged>>')
+            self._generate_changed_event()
+    
+    def __on_focus_out(self, event):
+         self._generate_changed_event()
 
     def __on_folder_btn_pressed(self):
         fname = None
---
+++ tests/test_pathchooserinput.py
@@ -0,0 +1,63 @@
+# encoding: utf8
+import os
+import sys
+import unittest
+try:
+    import tkinter as tk
+    import tkinter.ttk as ttk
+except:
+    import Tkinter as tk
+    import ttk
+
+
+pygubu_basedir = os.path.abspath(os.path.dirname(
+                    os.path.dirname(os.path.realpath(sys.argv[0]))))
+if pygubu_basedir not in sys.path:
+    sys.path.insert(0, pygubu_basedir)
+
+import pygubu
+import support
+from pygubu.widgets.pathchooserinput import PathChooserInput
+
+
+class TestPathChooserInput(unittest.TestCase):
+
+    def setUp(self):
+        support.root_deiconify()
+        xmldata = 'test_pathchooserinput.ui'
+        self.builder = builder = pygubu.Builder()
+        builder.add_from_file(xmldata)
+        self.mainwindow = builder.get_object('mainwindow')
+        self.widget = builder.get_object('pathchooserinput1')
+
+    def tearDown(self):
+        support.root_withdraw()
+
+    def test_class(self):
+        self.assertIsInstance(self.widget, PathChooserInput)
+        self.widget.destroy()
+        
+    def test_path_changed_event(self):
+        success = []
+
+        def on_path_changed(event=None):
+            success.append(1)
+
+        bag = { 'on_path_changed': on_path_changed }
+        self.builder.connect_callbacks(bag)
+
+        self.widget.configure(path='/new/path')
+        self.assertTrue(success)
+        self.widget.destroy()
+    
+    def test_type(self):
+        itype = str(self.widget.cget('type'))
+        self.assertEqual('file', itype)
+        self.widget.destroy()
+    
+    def test_path(self):
+        path = str(self.widget.cget('path'))
+        self.assertEqual('/home/user', path)
+        self.widget.destroy()
+    
+
