1d2df2aac52698d8a6ac3e23dbb412c66c8ae373
Alejandro Autalán <alejandroautalan@gmail.com>
2015-11-23 22:15:37 -0300

    Initial test for calendar, needs improvement.

---
+++ tests/test_calendar.py
@@ -0,0 +1,39 @@
+# encoding: utf8
+import os
+import sys
+import unittest
+try:
+    import tkinter as tk
+    import tkinter.ttk as ttk
+except:
+    import Tkinter as tk
+    import ttk
+
+
+pygubu_basedir = os.path.abspath(os.path.dirname(
+                    os.path.dirname(os.path.realpath(sys.argv[0]))))
+if pygubu_basedir not in sys.path:
+    sys.path.insert(0, pygubu_basedir)
+
+import pygubu
+import support
+from pygubu.widgets.calendarwidget import Calendar
+
+
+class TestCalendar(unittest.TestCase):
+
+    def setUp(self):
+        support.root_deiconify()
+        xmldata = 'test_calendar.ui'
+        self.builder = builder = pygubu.Builder()
+        builder.add_from_file(xmldata)
+        self.mainwindow = builder.get_object('mainwindow')
+        self.widget = builder.get_object('calendar')
+
+    def tearDown(self):
+        support.root_withdraw()
+
+    def test_class(self):
+        self.assertIsInstance(self.widget, Calendar)
+        self.widget.destroy()
+
