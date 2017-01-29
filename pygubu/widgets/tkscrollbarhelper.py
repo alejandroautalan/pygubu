1f729d8f597e0ede1bd68fc1a45c71c59bda3030
Alejandro Autalán <alejandroautalan@gmail.com>
2017-01-29 03:54:57 -0300

    Primera pruba de mousewheel support.

--- pygubu/widgets/tkscrollbarhelper.py
+++ pygubu/widgets/tkscrollbarhelper.py
@@ -17,6 +17,7 @@
 # For further info, check  https://github.com/alejandroautalan/pygubu
 
 from __future__ import unicode_literals
+import platform
 
 try:
     import tkinter as tk
@@ -40,11 +41,59 @@ def ScrollbarHelperFactory(frame_class, scrollbar_class):
         VERTICAL = 'vertical'
         HORIZONTAL = 'horizontal'
         BOTH = 'both'
+        # Mouse wheel support
+        __active_area = None
+        __init_binding = True
+        __OS = platform.system()
+        
+        @staticmethod
+        def on_mouse_wheel(event):
+            if ScrollbarHelper.__active_area:
+                ScrollbarHelper.__active_area.on_mouse_wheel(event)
+        
+        @staticmethod
+        def mouse_wheel_bind(widget):
+            ScrollbarHelper.__active_area = widget
+        
+        @staticmethod
+        def mouse_wheel_unbind():
+            ScrollbarHelper.__active_area = None
 
         def __init__(self, master=None, **kw):
             self.scrolltype = kw.pop('scrolltype', self.VERTICAL)
             super(ScrollbarHelper, self).__init__(master, **kw)
+            self.vsb = None
+            self.hsb = None
             self._create_scrollbars()
+            
+            if ScrollbarHelper.__init_binding == True:
+                
+                if self.__OS == "Linux" :
+                    master.bind_all('<4>', ScrollbarHelper.on_mouse_wheel,  add='+')
+                    master.bind_all('<5>', ScrollbarHelper.on_mouse_wheel,  add='+')
+                else:
+                    # Windows and MacOS
+                    master.bind_all("<MouseWheel>", ScrollbarHelper.on_mouse_wheel,  add='+')
+            ScrollbarHelper.__init_binding = False
+        
+        def _make_onmousewheel_cb(self, widget, orient, factor = 1):
+            view_command = getattr(widget, orient+'view')
+            if self.__OS == 'Linux':
+                def on_mouse_wheel(event):
+                    if event.num == 4:
+                        view_command("scroll",(-1)*factor,"units")
+                    elif event.num == 5:
+                        view_command("scroll",factor,"units") 
+            
+            elif self.__OS == 'Windows':
+                def on_mouse_wheel(event):        
+                    view_command("scroll",(-1)*int((event.delta/120)*factor),"units") 
+            
+            elif self.__OS == 'Darwin':
+                def on_mouse_wheel(event):        
+                    view_command("scroll",event.delta,"units")             
+            
+            return on_mouse_wheel
 
         def _create_scrollbars(self):
             if self.scrolltype in (self.BOTH, self.VERTICAL):
@@ -77,6 +126,21 @@ def ScrollbarHelperFactory(frame_class, scrollbar_class):
                 else:
                     msg = "widget {} has no attribute 'xview'".format(str(cwidget))
                     logger.warning(msg)
+            
+            if self.hsb and not hasattr(self.hsb, 'on_mouse_wheel'):
+                self.hsb.on_mouse_wheel = self._make_onmousewheel_cb(cwidget, 'x', 2)
+            if self.vsb and not hasattr(self.vsb, 'on_mouse_wheel'):
+                self.vsb.on_mouse_wheel = self._make_onmousewheel_cb(cwidget, 'y', 2)
+            
+            main_sb = self.vsb or self.hsb
+            if main_sb:
+                cwidget.on_mouse_wheel = main_sb.on_mouse_wheel
+                cwidget.bind('<Enter>', lambda event: ScrollbarHelper.mouse_wheel_bind(cwidget))
+                cwidget.bind('<Leave>', lambda event: ScrollbarHelper.mouse_wheel_unbind())
+            for s in (self.vsb, self.hsb):
+                if s:
+                    s.bind('<Enter>', lambda event, scrollbar=s: ScrollbarHelper.mouse_wheel_bind(scrollbar) )
+                    s.bind('<Leave>', lambda event: ScrollbarHelper.mouse_wheel_unbind())
 
     return ScrollbarHelper
 
