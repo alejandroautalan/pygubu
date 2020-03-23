7aebc030e30a439e4e791a15aea099a9a734cd7c
Alejandro Autalán <alejandroautalan@gmail.com>
2020-03-23 18:42:33 -0300

    Increase version number to sync with pypi.

--- pygubu/__init__.py
+++ pygubu/__init__.py
@@ -10,7 +10,7 @@ from pygubu.builder.builderobject import BuilderObject, register_widget
 from pygubu.binding import remove_binding, ApplicationLevelBindManager
 
 
-__version__ = '0.9.8.5'
+__version__ = '0.9.8.6'
 
 
 def register_property(name, description):
