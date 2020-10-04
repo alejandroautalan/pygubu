c09dbb4834487000c941b206c15df083b8f6e961
Alejandro Autalan <alejandroautalan@gmail.com>
2020-10-03 22:59:02 -0300

    Add dev utility.

---
+++ build-and-serve.sh
@@ -0,0 +1,8 @@
+python3 setup.py bdist_wheel
+cp dist/*.whl ../sharedfolder
+cd ..
+cd pygubu-designer
+python3 setup.py bdist_wheel
+cp dist/*.whl ../sharedfolder
+cd ../sharedfolder
+python3 -m http.server 8080
