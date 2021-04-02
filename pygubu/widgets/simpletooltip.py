dbf543e50ae7377f348e246b2009ed6c0a9fee04
Alejandro Autalan <alejandroautalan@gmail.com>
2021-04-01 22:30:40 -0300

    Fix tooltip position calculus. Add configuration options for label.

--- pygubu/widgets/simpletooltip.py
+++ pygubu/widgets/simpletooltip.py
@@ -11,11 +11,19 @@ except:
 
 class ToolTip(object):
 
-    def __init__(self, widget):
+    def __init__(self, widget, text=None, font=None,
+                background=None, foreground=None,
+                justify=None, wraplength=None):
         self.widget = widget
         self.tipwindow = None
         self.id = None
         self.x = self.y = 0
+        self.text = text if text is not None else '¿?'
+        self.font = font if font is not None else ("tahoma", "9", "normal")
+        self.background = background if background is not None else "#ffffe0"
+        self.foreground = foreground if foreground is not None else "black"
+        self.justify = justify if justify is not None else tk.LEFT
+        self.wraplength = wraplength if wraplength is not None else 300
     
     def inside_wbbox(self, rx , ry):
         bbox = self._calc_bbox(self.widget, True)
@@ -34,39 +42,55 @@ class ToolTip(object):
         y2 = ry + widget.winfo_height()
         return (rx, ry, x2, y2)
     
-    def _calc_final_pos(self):
+    def _calc_final_pos(self, ttwidth, ttheight):
         rx, ry, rcx, rcy = self._calc_bbox(self.widget, True)
         w = rcx-rx
         h = rcy-ry
-        sh = self.widget.winfo_screenheight() - 10
-        sw = self.widget.winfo_screenwidth() - 10
+        sh = self.widget.winfo_screenheight()
+        sw = self.widget.winfo_screenwidth()
         x = y = 0
-        for region in ('bottom', 'right', 'top', 'left'):
-            if region == 'bottom':
-                x = rx + int(w//2 * 0.2)
-                y = rcy + int(h//2 * 0.1)
-            elif region == 'right':
-                x = rcx + int(w//2 * 0.2)
-                y = ry + int(h//2 * 0.1)
-            elif region == 'top':
-                x = rx - int(w//2 * 0.4)
-                y = ry - int(h//2 * 0.4)
-            elif region == 'left':
+        #final_region = None
+        regions = ('se-al','se-ar', 'sw-al', 'sw-ar',
+                   'nw-al', 'nw-ar', 'ne-al', 'ne-ar')
+        for region in regions:
+            if region == 'ne-al':
                 x = rx
-                y = ry - 20
-            if x < sw and y < sh:
+                y = ry - ttheight
+            elif region == 'ne-ar':
+                x = rx - ttwidth
+                y = ry - ttheight
+            elif region == 'nw-al':
+                x = rx + w
+                y = ry - ttheight
+            elif region == 'nw-ar':
+                x = rx + w - ttwidth
+                y = ry - ttheight
+            elif region == 'se-al':
+                x = rx
+                y = ry + h
+            elif region == 'se-ar':
+                x = rx - ttwidth
+                y = ry + h
+            elif region == 'sw-al':
+                x = rx + w
+                y = ry + h
+            elif region == 'sw-ar':
+                x = rx + w - ttwidth
+                y = ry + h
+            x2 = x + ttwidth
+            y2 = y + ttheight
+            if (x > 0 and x2 < sw) and (y > 0 and y2 < sh):
+                #final_region = region
                 break
+        #print(final_region, x, x2, y, y2)
         return (x, y)
 
-    def showtip(self, text):
+    def showtip(self):
         "Display text in tooltip window"
-        self.text = text
         if self.tipwindow or not self.text:
             return
-        x, y = self._calc_final_pos()
         self.tipwindow = tw = tk.Toplevel(self.widget)
         tw.wm_overrideredirect(1)
-        tw.wm_geometry("+{0}+{1}".format(x, y))
         try:
             # For Mac OS
             tw.tk.call("::tk::unsupported::MacWindowStyle",
@@ -74,11 +98,14 @@ class ToolTip(object):
                        "help", "noActivates")
         except tk.TclError:
             pass
-        label = tk.Label(tw, text=self.text, justify=tk.LEFT,
-                      background="#ffffe0", foreground="black",
+        label = tk.Label(tw, text=self.text, justify=self.justify,
+                      background=self.background, foreground=self.foreground,
                       relief=tk.SOLID, borderwidth=1,
-                      font=("tahoma", "9", "normal"))
+                      font=self.font, wraplength=self.wraplength)
         label.pack(ipadx=2)
+        x, y = self._calc_final_pos(
+            label.winfo_reqwidth(), label.winfo_reqheight())
+        tw.wm_geometry("+{0}+{1}".format(x, y))
 
     def hidetip(self):
         tw = self.tipwindow
@@ -87,13 +114,14 @@ class ToolTip(object):
             tw.destroy()
 
 def create(widget, text):
-    toolTip = ToolTip(widget)
+    toolTip = ToolTip(widget, text)
     def enter(event):
-        toolTip.showtip(text)
+        toolTip.showtip()
     def leave(event):
         toolTip.hidetip()
     widget.bind('<Enter>', enter)
     widget.bind('<Leave>', leave)
+    return toolTip
 
 
 if __name__ == '__main__':
