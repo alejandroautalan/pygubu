6bc4ab05b095320d7134921ede156ba4d0533e40
Alejandro Autalan <alejandroautalan@gmail.com>
2021-09-05 22:30:52 -0300

    Upgrade version to sync with pypi.

--- pygubu/__init__.py
+++ pygubu/__init__.py
@@ -15,7 +15,7 @@ from pygubu.binding import ApplicationLevelBindManager, remove_binding
 from pygubu.builder import Builder
 from pygubu.builder.builderobject import BuilderObject, register_widget
 
-__version__ = '0.13'
+__version__ = '0.14'
 
 
 def register_property(name, description):
