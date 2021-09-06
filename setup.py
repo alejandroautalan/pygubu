6d209952a86ea9ae9cfec89bb4de7e45c2f0211e
Alejandro Autalan <alejandroautalan@gmail.com>
2021-09-05 22:30:26 -0300

    Use long description from readme file.

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ Needed packages to run (using Debian/Ubuntu package names):
 
     python3-tk
 """
+import os
+
 import pygubu
 
 try:
@@ -16,75 +18,8 @@ except ImportError:
 
 VERSION = pygubu.__version__
 
-long_description = \
-    """
-Welcome to pygubu a GUI designer for tkinter
-============================================
-
-Pygubu is a RAD tool to enable quick & easy development of user interfaces
-for the python tkinter module.
-
-The user interfaces designed are saved as XML, and by using the pygubu builder
-these can be loaded by applications dynamically as needed.
-Pygubu is inspired by Glade.
-
-IMPORTANT. Since version 0.10 pygubu was splited in two projects, the core library (this package)
-and the gui designer (pygubu-designer).
-
-
-Installation
-------------
-
-Pygubu requires python >= 2.7 (Tested only in python 2.7.3 and 3.2.3
-with tk8.5)
-
-Download and extract the tarball. Open a console in the extraction
-path and execute:
-
-::
-
-    python setup.py install
-
-
-Usage
------
-
-Create an UI definition using pygubu-designer and save it to a file. Then,
-create your aplication script as shown below. Note that 'mainwindow'
-is the name of your Toplevel widget.
-
-::
-
-    # helloworld.py
-    import tkinter as tk
-    import pygubu
-
-
-    class HelloWorldApp:
-
-        def __init__(self):
-
-            #1: Create a builder
-            self.builder = builder = pygubu.Builder()
-
-            #2: Load an ui file
-            builder.add_from_file('helloworld.ui')
-
-            #3: Create the mainwindow
-            self.mainwindow = builder.get_object('mainwindow')
-
-        def run(self):
-            self.mainwindow.mainloop()
-
-
-    if __name__ == '__main__':
-        app = HelloWorldApp()
-        app.run()
-
-
-See the examples directory or watch this hello world example on
-video http://youtu.be/wuzV9P8geDg
-"""
+_dirname_ = os.path.dirname(__file__)
+readme_path = os.path.join(_dirname_, 'README.md')
 
 setup(
     name='pygubu',
@@ -93,7 +28,8 @@ setup(
     author='Alejandro Autalán',
     author_email='alejandroautalan@gmail.com',
     description='A tkinter GUI builder.',
-    long_description=long_description,
+    long_description=open(readme_path, 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/alejandroautalan/pygubu',
 
     packages=['pygubu', 'pygubu.builder',
