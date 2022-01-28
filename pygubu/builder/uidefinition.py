92527156acfefcc84e139e3519ae72f64e726abd
Alejandro Autalan <alejandroautalan@gmail.com>
2022-01-27 22:00:36 -0300

    Write node for builders that not require layout but can do container_layout.

--- pygubu/builder/uidefinition.py
+++ pygubu/builder/uidefinition.py
@@ -343,8 +343,7 @@ class UIDefinition(object):
 
         # Container layout properties
         container_layout_required = (
-            layout_required
-            and (wmeta.container_properties or wmeta.gridrc_properties)
+            wmeta.container_properties or wmeta.gridrc_properties
         )
         if container_layout_required:
             # create layout node
