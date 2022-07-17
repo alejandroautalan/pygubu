f3687d3f88a9c2393c186308bee657aef3ffef6b
Alejandro Autalan <alejandroautalan@gmail.com>
2022-07-17 17:40:04 -0300

    save this test for future use.

---
+++ tests/test_builderfont.py
@@ -0,0 +1,51 @@
+# encoding: utf8
+import os
+import sys
+import unittest
+import tkinter as tk
+import tkinter.ttk as ttk
+import tkinter.font as tkfont
+
+pygubu_basedir = os.path.abspath(os.path.dirname(
+    os.path.dirname(os.path.realpath(sys.argv[0]))))
+if pygubu_basedir not in sys.path:
+    sys.path.insert(0, pygubu_basedir)
+
+import pygubu
+import support
+
+
+class TestBuilderFont(unittest.TestCase):
+
+    def setUp(self):
+        support.root_deiconify()
+        xmldata = "test_builderfont.ui"
+        self.builder = builder = pygubu.Builder()
+        builder.add_from_file(xmldata)
+
+        self.font = font = tkfont.Font(
+            family='Helvetica', size=32, weight='bold')
+        builder.set_font('custom_font', font)
+
+        self.widget = builder.get_object('mainwindow')
+
+    def tearDown(self):
+        support.root_withdraw()
+
+    @unittest.skip("Not implemented yet")
+    def test_get_font(self):
+        font = self.builder.get_font('custom_font')
+        self.assertEqual(self.font, font)
+        self.widget.destroy()
+
+    @unittest.skip("Not implemented yet")
+    def test_get_font_notset(self):
+        fname = 'other_font'
+        font = self.builder.get_font(fname)
+        self.assertEqual(fname, font)
+        self.widget.destroy()
+
+
+if __name__ == '__main__':
+    unittest.main()
+
