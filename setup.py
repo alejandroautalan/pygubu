39ee018a09f0b5966954402d3c4d0871e08410ce
Alejandro Autalan <alejandroautalan@gmail.com>
2022-07-19 02:06:17 -0300

    Find all packages. Add extra requirements file (for installing plugins?)

--- setup.py
+++ setup.py
@@ -7,20 +7,42 @@ Needed packages to run (using Debian/Ubuntu package names):
 
     python3-tk
 """
-import os
+import pathlib
 from io import open
 
 import pygubu
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
 VERSION = pygubu.__version__
 
-_dirname_ = os.path.dirname(__file__)
-readme_path = os.path.join(_dirname_, "README.md")
+
+def get_extra_requires(path, add_all=True):
+    import re
+    from collections import defaultdict
+
+    with open(path) as fp:
+        extra_deps = defaultdict(set)
+        for k in fp:
+            if k.strip() and not k.startswith("#"):
+                tags = set()
+                if ":" in k:
+                    k, v = k.split(":")
+                    tags.update(vv.strip() for vv in v.split(","))
+                tags.add(re.split("[<=>]", k)[0])
+                for t in tags:
+                    extra_deps[t].add(k)
+
+        # add tag `all` at the end
+        if add_all:
+            extra_deps["all"] = set(vv for v in extra_deps.values() for vv in v)
+
+    return extra_deps
+
+
+_dirname_ = pathlib.Path(__file__).parent
+readme_path = _dirname_ / "README.md"
+extra_req_path = _dirname_ / "extra_requirements.txt"
 
 setup(
     name="pygubu",
@@ -32,12 +54,7 @@ setup(
     long_description=open(readme_path, "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/alejandroautalan/pygubu",
-    packages=[
-        "pygubu",
-        "pygubu.builder",
-        "pygubu.builder.widgets",
-        "pygubu.widgets",
-    ],
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -49,4 +66,5 @@ setup(
         "Topic :: Software Development :: User Interfaces",
     ],
     python_requires=">=3.6",
+    extras_require=get_extra_requires(extra_req_path),
 )
