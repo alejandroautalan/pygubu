58d3bd2d7013b925a9f0d0c60afbd719ef3333c9
Alejandro Autalan <alejandroautalan@gmail.com>
2022-07-20 00:51:34 -0300

    Remove mixin class, not needed anymore.

--- pygubu/plugins/awesometkinter/awesomebuilders.py
+++ pygubu/plugins/awesometkinter/awesomebuilders.py
@@ -3,43 +3,26 @@ Documentation, License etc.
 
 @package pygubu.plugins.awesometkinter
 """
-import tkinter as tk
 from pygubu.i18n import _
 from pygubu.api.v1 import BuilderObject, register_widget
 from pygubu.plugins.tk.tkstdwidgets import TKFrame, TKCheckbutton
 from pygubu.plugins.ttk.ttkstdwidgets import TTKButton, TTKFrame, TTKRadiobutton
 import awesometkinter as atk
 
-designer_tab_label = "AwesomeTkinter"
+designer_tab_label = _("AwesomeTkinter")
 module_uid = "awesometkinter"
 
 
-class ATKInitializeMixin:
-    def atk_initialize(self, master):
-        root = master
-        if not isinstance(master, tk.Toplevel) or not isinstance(master, tk.Tk):
-            root = master.winfo_toplevel()
-        root.config(background=atk.DEFAULT_COLOR)
-
-
-class Frame3dBO(TTKFrame, ATKInitializeMixin):
+class Frame3dBO(TTKFrame):
     class_ = atk.Frame3d
-
-    def realize(self, parent):
-        self.atk_initialize(parent.get_child_master())
-        super(Frame3dBO, self).realize(parent)
 
 
 _builder_uid = module_uid + ".frame3d"
 register_widget(_builder_uid, Frame3dBO, "Frame3d", ("ttk", designer_tab_label))
 
 
-class ScrollableFrameBO(TKFrame, ATKInitializeMixin):
+class ScrollableFrameBO(TKFrame):
     class_ = atk.ScrollableFrame
-
-    def realize(self, parent):
-        self.atk_initialize(parent.get_child_master())
-        super(ScrollableFrameBO, self).realize(parent)
 
     def _get_init_args(self):
         # this widget does not support class_ wk argument
