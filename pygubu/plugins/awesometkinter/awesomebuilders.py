1367d090bf39ee4c2096e4ead27799fd8d52a09d
Alejandro Autalan <alejandroautalan@gmail.com>
2022-07-20 01:59:20 -0300

    Set properties.

--- pygubu/plugins/awesometkinter/awesomebuilders.py
+++ pygubu/plugins/awesometkinter/awesomebuilders.py
@@ -4,7 +4,11 @@ Documentation, License etc.
 @package pygubu.plugins.awesometkinter
 """
 from pygubu.i18n import _
-from pygubu.api.v1 import BuilderObject, register_widget
+from pygubu.api.v1 import (
+    BuilderObject,
+    register_widget,
+    register_custom_property,
+)
 from pygubu.plugins.tk.tkstdwidgets import TKFrame, TKCheckbutton
 from pygubu.plugins.ttk.ttkstdwidgets import TTKButton, TTKFrame, TTKRadiobutton
 import awesometkinter as atk
@@ -14,11 +18,18 @@ module_uid = "awesometkinter"
 
 
 class Frame3dBO(TTKFrame):
+    OPTIONS_STANDARD = tuple(set(TTKFrame.OPTIONS_STANDARD) - set(("style",)))
+    OPTIONS_CUSTOM = ("bg",)
+    properties = OPTIONS_STANDARD + TTKFrame.OPTIONS_SPECIFIC + OPTIONS_CUSTOM
+    ro_properties = TTKFrame.ro_properties + ("bg",)
     class_ = atk.Frame3d
 
 
 _builder_uid = module_uid + ".frame3d"
 register_widget(_builder_uid, Frame3dBO, "Frame3d", ("ttk", designer_tab_label))
+
+_help = _("Background color")
+register_custom_property(_builder_uid, "bg", "colorentry", help=_help)
 
 
 class ScrollableFrameBO(TKFrame):
