ee9ec0363983058849feda72b8f78f6063d6e087
Alejandro Autalan <alejandroautalan@gmail.com>
2022-07-25 02:46:40 -0300

    change name.

--- pygubu/plugins/ttkwidgets/__init__.py
+++ pygubu/plugins/ttkwidgets/__init__.py
@@ -2,7 +2,7 @@ import importlib
 from pygubu.i18n import _
 from pygubu.api.v1 import BuilderLoaderPlugin
 
-_designer_tab_label = _("TtkWidgets")
+_designer_tab_label = _("ttkwidgets")
 _plugin_uid = "ttkwidgets"
 
 
