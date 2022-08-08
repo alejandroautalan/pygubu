ab4a85d80a6696345fe9ef320d26d8cc60ef5b78
Alejandro Autalan <alejandroautalan@gmail.com>
2022-08-08 18:23:41 -0300

    Remove unused files.

--- test-dist.sh
+++
@@ -1,4 +0,0 @@
-rm dist/* -rf
-python setup.py sdist
-cd dist/; tar xvzf pygubu-*.tar.gz ; cd pygubu-*; cpython3.4 setup.py install; cd ..; cd ..;
-cpython3.4 ~/apps/cpython-3.4.0/bin/pygubu-designer
