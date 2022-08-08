114423d1dacf2a4814949af3a15d850749af0212
Alejandro Autalan <alejandroautalan@gmail.com>
2022-08-08 19:27:02 -0300

    Add extra requirements in setup.

--- setup.py
+++ setup.py
@@ -16,33 +16,25 @@ from setuptools import setup, find_packages
 
 VERSION = pygubu.__version__
 
-
-def get_extra_requires(path, add_all=True):
-    import re
-    from collections import defaultdict
-
-    with open(path) as fp:
-        extra_deps = defaultdict(set)
-        for k in fp:
-            if k.strip() and not k.startswith("#"):
-                tags = set()
-                if ":" in k:
-                    k, v = k.split(":")
-                    tags.update(vv.strip() for vv in v.split(","))
-                tags.add(re.split("[<=>]", k)[0])
-                for t in tags:
-                    extra_deps[t].add(k)
-
-        # add tag `all` at the end
-        if add_all:
-            extra_deps["all"] = set(vv for v in extra_deps.values() for vv in v)
-
-    return extra_deps
-
-
 _dirname_ = pathlib.Path(__file__).parent
 readme_path = _dirname_ / "README.md"
-extra_req_path = _dirname_ / "extra_requirements.txt"
+extras_require = {
+    "ttkwidgets": {"ttkwidgets"},
+    "tksheet": {"tksheet"},
+    "tkinterweb": {"tkinterweb"},
+    "tkintertable": {"tkintertable"},
+    "tkcalendar": {"tkcalendar"},
+    "AwesomeTkinter": {"AwesomeTkinter"},
+    "awesometkinter": {"AwesomeTkinter"},
+    "all": {
+        "AwesomeTkinter",
+        "tkcalendar",
+        "tkintertable",
+        "tkinterweb",
+        "tksheet",
+        "ttkwidgets",
+    },
+}
 
 setup(
     name="pygubu",
@@ -66,5 +58,5 @@ setup(
         "Topic :: Software Development :: User Interfaces",
     ],
     python_requires=">=3.6",
-    extras_require=get_extra_requires(extra_req_path),
+    extras_require=extras_require,
 )
