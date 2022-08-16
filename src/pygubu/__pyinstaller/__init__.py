d733987c905d6886c694de390ab31cfbc9612468
Geoffrey Welch <gwelch@contegix.com>
2022-08-16 16:57:07 -0700

    Add PyInstaller hook

---
+++ src/pygubu/__pyinstaller/__init__.py
@@ -0,0 +1,5 @@
+import os
+
+
+def get_hook_dirs() -> list[str]:
+    return [os.path.dirname(__file__)]
---
+++ src/pygubu/__pyinstaller/hook-pygubu.py
@@ -0,0 +1,11 @@
+hiddenimports = [
+    'pygubu.plugins.tk.tkstdwidgets',
+    'pygubu.plugins.ttk.ttkstdwidgets',
+    'pygubu.plugins.pygubu.dialog',
+    'pygubu.plugins.pygubu.editabletreeview',
+    'pygubu.plugins.pygubu.scrollbarhelper',
+    'pygubu.plugins.pygubu.scrolledframe',
+    'pygubu.plugins.pygubu.tkscrollbarhelper',
+    'pygubu.plugins.pygubu.tkscrolledframe',
+    'pygubu.plugins.pygubu.pathchooserinput',
+]
