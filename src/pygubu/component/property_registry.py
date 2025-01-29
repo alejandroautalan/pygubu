c27a8d8c3a5d8a863f7f84db05e16be87b5e79bd
Alejandro Autalan <alejandroautalan@gmail.com>
2025-01-29 00:08:03 -0300

    Allow designer to listen to new properties created. refs alejandroautalan/pygubu-designer#284

--- src/pygubu/component/property_registry.py
+++ src/pygubu/component/property_registry.py
@@ -1,14 +1,84 @@
+import os
 import logging
+
+from typing import Optional
+from abc import ABC, abstractmethod
 
 
 logger = logging.getLogger(__name__)
 
 
-class PropertyRegistryBase:
+class IPropertyRegistry(ABC):
+    def __init__(self):
+        self.properties = {}
+
+    def register(self, name: str, description: dict):
+        if name in self.properties:
+            self._update_existent(name, description)
+        else:
+            self._add_new(name, description)
+
+    @abstractmethod
+    def _add_new(self, name: str, description: dict):
+        ...
+
+    @abstractmethod
+    def _update_existent(self: str, name, description: dict):
+        ...
+
+    @abstractmethod
+    def iter_names(self):
+        ...
+
+    @abstractmethod
+    def register_custom(
+        self,
+        builder_uid: str,
+        prop_name: str,
+        editor: str,
+        default_value=None,
+        help=None,
+        **editor_params,
+    ):
+        ...
+
+
+class PropertyRegistryDummy(IPropertyRegistry):
+    """If designer is not running there is no need to maintain
+    more structures in memory. This class will ignore all method calls.
+    """
+
+    def _add_new(self, name: str, description: dict):
+        logger.debug("New property %s, ignored.", name)
+
+    def _update_existent(self: str, name, description: dict):
+        logger.debug("Update property %s, ignored.", name)
+
+    def iter_names(self):
+        return None
+
+    def register_custom(
+        self,
+        builder_uid: str,
+        prop_name: str,
+        editor: str,
+        default_value=None,
+        help=None,
+        **editor_params,
+    ):
+        return None
+
+    def copy_to_builder(
+        self, from_builder_id: str, pname: str, to_builder_id: str
+    ):
+        return None
+
+
+class PropertyRegistryBase(IPropertyRegistry):
     """Mantain and manage property definitions."""
 
     def __init__(self):
-        self.properties: dict = {}
+        super().__init__()
         self._is_sorted: bool = False
         self._sorted_names: list[str] = None
 
@@ -71,4 +141,19 @@ class PropertyRegistryBase:
             self.properties[pname][to_builder_id] = description
 
 
-PropertyRegistry = PropertyRegistryBase()
+if "PYGUBU_DESIGNER_RUNNING" in os.environ:
+    from blinker import Signal
+
+    class PropertyRegistryWithSignals(PropertyRegistryBase):
+        on_property_new = Signal()
+        # on_property_update = signal()
+
+        def _add_new(self, name: str, description: dict):
+            super()._add_new(name, description)
+            if self.on_property_new.receivers:
+                self.on_property_new.send(name=name)
+
+    PropertyRegistry = PropertyRegistryWithSignals()
+
+else:
+    PropertyRegistry = PropertyRegistryDummy()
