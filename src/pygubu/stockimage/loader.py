89cae4f858db6b9b02c579942c0a762ca6895a80
Alejandro Autalan <alejandroautalan@gmail.com>
2025-03-04 21:07:45 -0300

    Add option for auto scale images for high DPI screens. Only scales tk.PhotoImages on loading time.

--- src/pygubu/stockimage/loader.py
+++ src/pygubu/stockimage/loader.py
@@ -1,4 +1,6 @@
 import logging
+import math
+import tkinter as tk
 from .exceptions import ImageFormatNotSupportedError, StockImageException
 from .registry import StockRegistry, StockItem
 from .config import _img_notsupported
@@ -9,8 +11,11 @@ logger = logging.getLogger(__name__)
 
 class StockImageCache:
     IMAGE_NOT_SUPPORTED = "img_not_supported"
+    BASELINE = 1.33398982438864281
 
-    def __init__(self, tkroot, registry: StockRegistry):
+    def __init__(
+        self, tkroot, registry: StockRegistry, *, auto_scaling: bool = False
+    ):
         self.tkroot = tkroot
         self.registry: StockRegistry = registry
         self._cached = {}
@@ -18,6 +23,8 @@ class StockImageCache:
             self.register_from_data(
                 self.IMAGE_NOT_SUPPORTED, "gif", _img_notsupported
             )
+        self.auto_scaling = auto_scaling
+        self._scale_factor = None
 
     def clear_cache(self):
         """Call this before closing tk root"""
@@ -76,6 +83,10 @@ class StockImageCache:
         else:
             raise StockImageException(f"StockImage: {image_id} not registered.")
 
+    def _setup_root(self):
+        if self.tkroot is None:
+            self.tkroot = tk._get_default_root()
+
     def _load_image(self, image_id, custom_loader=None):
         """Load image from file or return the cached instance."""
 
@@ -85,6 +96,8 @@ class StockImageCache:
             img = stock_item.create_image(
                 tk_master=self.tkroot, custom_loader=custom_loader
             )
+            if isinstance(img, tk.PhotoImage):
+                img = self._process_photo_image(img)
         except ImageFormatNotSupportedError:
             msg = "Error loading image %s, try installing Pillow module."
             logger.error(msg, image_id)
@@ -93,6 +106,49 @@ class StockImageCache:
         self._cached[image_id] = img
         logger.info("Loaded resource data for %s.", image_id)
         return img
+
+    @property
+    def scale_factor(self):
+        if self._scale_factor is None:
+            self._setup_root()
+            scaling = self.tkroot.tk.call("tk", "scaling")
+            self._scale_factor = scaling / self.BASELINE
+        return self._scale_factor
+
+    def _process_photo_image(self, image: tk.PhotoImage):
+        """Additional processing for image before adding to cache."""
+        new_image = image
+        if self.auto_scaling:
+            new_image = self._resize_photo(image, self.scale_factor)
+        return new_image
+
+    def _resize_photo(self, img: tk.PhotoImage, scale_factor: float):
+        """Resize image using tk.PhotoImage methods.
+        scale_factor is rounded to the nearest 0.5 value.
+        Resizing starts if scale factor is greater or equal to 1.5,
+        otherwise image is returned with no changes.
+        """
+        w = img.width()
+        new_w = math.ceil(w * scale_factor)
+        # h = img.height()
+        # new_h = math.ceil(h * scale_factor)
+
+        # round(number / roundto) * roundto
+        xfactor = round((new_w / w) / 0.5) * 0.5
+        if xfactor < 1.5:
+            # No scaling for xfactor below 1.5
+            return img
+        xfactor_par = xfactor % 2 == 0.0
+        if xfactor_par:
+            # zoom scaling
+            zoom_factor = int(xfactor)
+            new_img = img.zoom(zoom_factor)
+        else:
+            # zoom + subsample scaling
+            zoom_factor = int(xfactor * 2)
+            zimg = img.zoom(zoom_factor)
+            new_img = zimg.subsample(2)
+        return new_img
 
     def as_iconbitmap(self, image_id):
         """Get image path for use in iconbitmap property"""
