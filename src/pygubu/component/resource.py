3cd03261856eb4e69b648831679deeebc31846b7
Alejandro Autalan <alejandroautalan@gmail.com>
2025-03-24 01:12:30 -0300

    Add chained resouce pool.

--- src/pygubu/component/resource.py
+++ src/pygubu/component/resource.py
@@ -1,11 +1,16 @@
 from abc import ABC, abstractmethod
-from typing import Optional, Any
+from typing import Optional, Any, List
 from urllib.parse import urlparse
 from functools import reduce
+from enum import Enum, auto
 
 
 class ResourceError(Exception):
     ...
+
+
+class ResourceValue(Enum):
+    UNDEFINED = auto()
 
 
 class ResouceDefinition:
@@ -14,14 +19,22 @@ class ResouceDefinition:
         self.meta = {} if meta is None else meta
 
     def __repr__(self):
-        return f"ResouceDefinition({self.uid}, {self.meta})"
+        return f"{type(self).__name__}({self.uid}, {self.meta})"
 
 
 class IDataPool(ABC):
     """A generic pool of data."""
 
     @abstractmethod
-    def get_resource(self, uri: str) -> Any:
+    def get_resource(
+        self, uri: str, *, default_value=ResourceValue.UNDEFINED, **kw
+    ) -> Any:
+        """Get resource value from pool.
+
+        If value for uri is not defined, must return ResourceValue.UNDEFINED
+        If uri is invalid, must return ResourceError
+        For any other error must return ResourceError
+        """
         ...
 
     def get_uri_path(self, uri: str) -> str:
@@ -38,11 +51,13 @@ class DictDataPool(IDataPool):
     def __init__(self, data: dict = None):
         self.data = {} if data is None else data
 
-    def get_resource(self, uri: str) -> Any:
+    def get_resource(
+        self, uri: str, *, default_value=ResourceValue.UNDEFINED, **kw
+    ) -> Any:
         path = self.get_uri_path(uri)
-        return self.deep_get(self.data, path)
+        return self.deep_get(self.data, path, default_value)
 
-    def deep_get(self, dictionary, keys, default=None):
+    def deep_get(self, dictionary, keys, default):
         return reduce(
             lambda d, key: d.get(key, default)
             if isinstance(d, dict)
@@ -50,6 +65,35 @@ class DictDataPool(IDataPool):
             keys.split("/"),
             dictionary,
         )
+
+
+class ChainedResoucePool(IDataPool):
+    def __init__(self, chain: List[IDataPool]):
+        self.chain = chain
+
+    def get_resource(
+        self, uri: str, *, default_value=ResourceValue.UNDEFINED, **kw
+    ) -> Any:
+        value = ResourceValue.UNDEFINED
+        for rpool in self.chain:
+            value = rpool.get_resource(uri, **kw)
+            is_undefined = (
+                isinstance(value, ResourceValue)
+                and value == ResourceValue.UNDEFINED
+            )
+            if not is_undefined:
+                break
+        is_undefined = (
+            isinstance(value, ResourceValue)
+            and value == ResourceValue.UNDEFINED
+        )
+        if is_undefined:
+            value = default_value
+        return value
+
+
+class PygubuResourcePool(IDataPool):
+    ...
 
 
 if __name__ == "__main__":
@@ -67,3 +111,26 @@ if __name__ == "__main__":
             print(res)
         except ResourceError:
             print("Invalid uri error:", uri)
+
+    print("Chained test:")
+    data1 = dict(dog_age=5)
+    data2 = dict(dog_age=10, dog_name="generic dog")
+    pool_a = DictDataPool(data1)
+    pool_b = DictDataPool(data2)
+    pool_chain = ChainedResoucePool([pool_a, pool_b])
+    rlist = [
+        "res://dog_age",
+        "res://dog_name",
+        "res://dog_failed",
+        "ftp//invalid_uri",
+    ]
+    for uri in rlist:
+        try:
+            res = pool_chain.get_resource(uri)
+            print(res)
+        except ResourceError:
+            print("Invalid uri error:", uri)
+    print("Test default value:")
+    uri = "res://dog_not_exist"
+    value = pool_chain.get_resource(uri, default_value=None)
+    print(f"Value for {uri} -> {value}")
